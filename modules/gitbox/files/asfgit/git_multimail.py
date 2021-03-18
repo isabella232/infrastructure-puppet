f6d2ad97156272b6425bb7d191f8d50500f62229
Daniel Gruno <humbedooh@apache.org>
2021-03-18 16:04:44 +0100

    typo

--- modules/gitbox/files/asfgit/git_multimail.py
+++ modules/gitbox/files/asfgit/git_multimail.py
@@ -329,7 +329,7 @@ in repository https://gitbox.apache.org/repos/asf/%(repo_shortname)s.git.
 
 """
 
-# N.B. the traiing space following '--' below is intentional
+# N.B. the trailing space following '--' below is intentional
 FOOTER_TEMPLATE = """\
 
 
