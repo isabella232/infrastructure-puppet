62f3a1232d7469b47421cd056ecd3bd86ff936c8
dfoulks <dfoulks@apache.org>
2021-04-06 13:32:23 -0500

    Puppet 3 Clean up
    drat-vm.apache.org
    hermes-vm.apache.org
    mergebot-vm2.apache.org
    ponymail-vm.apache.org
    
    aardvark_asf
    allura_pvm_asf
    blogs_asf
    cordova_pvm_asf
    crowd_asf
    cwiki_asf
    dist_archive_asf
    fisheye_asf
    forrest_pvm_asf
    gump_server
    jira_asf
    jira_slack_bridge
    netbeans_pvm_asf
    pad_asf
    projects_pvm_asf
    spamassassin_pvm_asf
    svnqavm_pvm_asf
    tlp_vhosts
    gitwcsub

--- modules/tlp_vhosts/files/zzz/mirror-tests/Readme.txt
+++
@@ -1,3 +0,0 @@
-These files are just randomly generated files.
-They are for testing purposes only, ignore.
-
--- modules/tlp_vhosts/manifests/init.pp
+++
@@ -1,9 +0,0 @@
-#/etc/puppet/modules/tlp_vhosts/manifests.init.pp
-
-class tlp_vhosts (
-){
-
-    anchor { 'tlp_vhosts::begin': }
-    -> class { '::tlp_vhosts::compat': }
-    -> anchor { 'tlp_vhosts::end': }
-}
