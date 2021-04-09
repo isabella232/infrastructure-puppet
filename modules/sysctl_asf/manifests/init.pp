68fa29bcdea2dda0c116aafc5ee7b3f65826a796
dfoulks <dfoulks@apache.org>
2021-04-09 08:45:04 -0500

    Puppet3 clean out defunct modules
    Removed the following:
    aim_server
    ghmon_server
    lucene1_pvm_asf
    maven_central_mirror_asf
    mx_asf
    nexus_asf
    ofbiz_pvm_asf
    pdns_recursor
    pflogsumm
    pootle_asf
    postfix_server_asf
    qmail_asf
    reviews_asf
    rsync_bai_asf
    rsync_mirror
    selfserve
    selfserve_portal
    svngit2jira
    sysctl_asf
    tomcat_asf
    vcsrepo_asf
    vhosts_whimsy
    whimsy_server
    wicket_pvm_asf
    wiki_asf

--- modules/sysctl_asf/manifests/init.pp
+++
@@ -1,8 +0,0 @@
-#/etc/puppet/modules/sysctl/manifests/init.pp
-
-class sysctl_asf (
-) {
-
-  $sysctl = hiera_hash('sysctl',{})
-  create_resources(sysctl, $sysctl)
-}
--- modules/vcsrepo_asf/manifests/init.pp
+++
@@ -1,7 +0,0 @@
-#/etc/puppet/modules/vcsrepo_asf/manifests/init.pp
-
-class vcsrepo_asf {
-  $vcsrepo = hiera_hash('vcsrepo',{})
-  create_resources(vcsrepo, $vcsrepo)
-}
-
